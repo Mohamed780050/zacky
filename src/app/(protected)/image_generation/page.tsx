fc20b0893756d557000ffe2720b8a95272d80f01
Mohamed780050 <mohamed39omer@gmail.com>
2025-08-10 18:53:09 +0300

    added video and image headline

--- src/app/(protected)/image_generation/page.tsx
+++ src/app/(protected)/image_generation/page.tsx
@@ -1,4 +1,16 @@
+import Heading from "@/features/root/_components/Heading";
+import { ImageIcon } from "lucide-react";
+
 function page() {
-  return <div>conversation</div>;
+  return (
+    <div>
+      <Heading
+        title="Image Generation"
+        description="talk with Zacky Advanced AI conversational agent."
+        icon={<ImageIcon className="h-10 w-10 text-pink-500" />}
+        bgColor="bg-pink-500/10"
+      />
+    </div>
+  );
 }
 export default page;
--- src/app/(protected)/video_generation/page.tsx
+++ src/app/(protected)/video_generation/page.tsx
@@ -1,4 +1,16 @@
+import Heading from "@/features/root/_components/Heading";
+import { VideoIcon } from "lucide-react";
+
 function page() {
-  return <div>conversation</div>;
+  return (
+    <div>
+      <Heading
+        title="Video Generation"
+        description="Generate images using Zacky Advanced AI."
+        icon={<VideoIcon className="h-10 w-10 text-orange-700" />}
+        bgColor="bg-orange-700/10"
+      />
+    </div>
+  );
 }
 export default page;
