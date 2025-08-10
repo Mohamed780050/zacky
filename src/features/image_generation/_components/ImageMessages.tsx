4e23bdf3f670a8df25fc1b7e872625e64908ebab
Mohamed780050 <mohamed39omer@gmail.com>
2025-08-10 19:13:26 +0300

    worked on the interface of the image generation

---
+++ src/features/image_generation/_components/ImageMessages.tsx
@@ -0,0 +1,16 @@
+import Empty from "@/features/root/_components/Empty";
+import { ImageIcon, MessageSquare } from "lucide-react";
+
+function ImageMessages() {
+  return (
+    <div>
+      <Empty
+        label="How can I help you today?"
+        description="Start a conversation by typing a message below."
+        icon={<ImageIcon className="h-15 w-15 text-pink-500" />}
+        bgColor="bg-pink-500/10"
+      />
+    </div>
+  );
+}
+export default ImageMessages;
