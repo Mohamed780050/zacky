797f984eee37ee721c3d80e42e064bce78191a3d
Mohamed780050 <mohamed39omer@gmail.com>
2025-10-14 16:49:15 +0300

    made code gen to decrease the freetrail

--- src/features/code_generation/actions/CodeActions.ts
+++ src/features/code_generation/actions/CodeActions.ts
@@ -5,6 +5,7 @@ import { flattenError } from "zod";
 import { sendCode } from "@/lib/sendMessages";
 import { GenerateCodeWithModel } from "@/lib/communicateWithModels";
 import { revalidatePath } from "next/cache";
+import { CheckLimitation, decreaseFreeTrailCount } from "@/lib/api-limit";
 
 export async function CodeSubmit(
   prevState: ConversationActionState,
@@ -21,6 +22,13 @@ export async function CodeSubmit(
     }
 
     const { prompt } = parsedData.data;
+    const stillCanUseFreeTrial = await CheckLimitation();
+
+    if (!stillCanUseFreeTrial)
+      throw { status: 403, message: "free trail is over" };
+
+    await decreaseFreeTrailCount();
+    
     const data = await GenerateCodeWithModel(prompt);
 
     await sendCode(prompt, `${data?.candidates?.[0]?.content?.parts[0].text}`);
