1e4e0ffbbafe752162d2ea4ef57e32fb26a7c2f4
Mohamed780050 <mohamed39omer@gmail.com>
2025-10-14 20:51:09 +0300

    improved the comming soon bannar

--- src/features/home/ComingSoonBanner.tsx
+++ src/features/home/ComingSoonBanner.tsx
@@ -1,6 +1,6 @@
 export default function ComingSoonBanner() {
   return (
-    <div className="bg-gradient-to-r from-orange-400 to-pink-500 text-white p-2 rounded-2xl">
+    <div className="rounded-2xl bg-gradient-to-r from-orange-400 to-pink-500 p-2 text-nowrap text-white">
       Coming Soon
     </div>
   );
