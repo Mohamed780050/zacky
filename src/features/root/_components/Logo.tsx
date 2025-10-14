536f8f929db20d59bfa91df098f34f7526335299
Mohamed780050 <mohamed39omer@gmail.com>
2025-10-14 15:32:24 +0300

    changed logo

--- src/features/root/_components/Logo.tsx
+++ src/features/root/_components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Logo() {
   return (
     <div className="relative w-8 h-8 mr-4">
-      <Image fill src="/logo.svg" alt="logo" />
+      <Image fill src="/logo.png" alt="logo" />
     </div>
   );
 }
