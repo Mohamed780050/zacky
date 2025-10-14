cac0915125cb4c0278119988878226e192d22446
Mohamed780050 <mohamed39omer@gmail.com>
2025-10-14 15:44:29 +0300

    changed logo width and height

--- src/features/root/_components/Logo.tsx
+++ src/features/root/_components/Logo.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 function Logo() {
   return (
-    <div className="relative w-8 h-8 mr-4">
-      <Image fill src="/logo.png" alt="logo" className="w-8 h-8"/>
+    <div className="relative mr-2 h-24 w-24">
+      <Image fill src="/logo.png" alt="logo" />
     </div>
   );
 }
