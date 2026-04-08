baf83309ea2592285fd933e012849f6be6d3d78f
Mohamed780050 <mohamed39omer@gmail.com>
2026-04-08 17:47:40 +0200

    added internationalzation to the project

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,8 +4,13 @@ import {
 } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import ModalProvider from "@/components/ui/modal-proivder";
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { arSA, enUS } from '@clerk/localizations';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,19 +36,31 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
+  params
 }: Readonly<{
   children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }>) {
+  const { locale } = await params;
+  
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
+
+  const messages = await getMessages();
+
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang={locale} className="scroll-smooth" dir={locale === 'ar' ? 'rtl' : 'ltr'}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider appearance={{ baseTheme: dark }}>
-          <ModalProvider />
-          {children}
+        <ClerkProvider localization={locale === 'ar' ? arSA : enUS} appearance={{ baseTheme: dark }}>
+          <NextIntlClientProvider messages={messages}>
+            <ModalProvider />
+            {children}
+          </NextIntlClientProvider>
         </ClerkProvider>
       </body>
     </html>
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,25 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/","/pricing"]);
+const intlMiddleware = createMiddleware(routing);
+
+const isPublicRoute = createRouteMatcher([
+  "/:locale/sign-in(.*)", 
+  "/:locale/sign-up(.*)", 
+  "/:locale",
+  "/:locale/pricing",
+  "/sign-in(.*)", 
+  "/sign-up(.*)", 
+  "/",
+  "/pricing"
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
+  return intlMiddleware(req);
 });
 
 export const config = {
